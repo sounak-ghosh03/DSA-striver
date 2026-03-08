97fd526673a60e0503b88c91641b8f5bee0bc61f
SOUNAK GHOSH <sounak2309@gmail.com>
2026-03-08 20:22:54 +0530

    hashing and maps

--- 01_Basics/hash.cpp
+++ 01_Basics/hash.cpp
@@ -2,97 +2,100 @@
 
 using namespace std;
 
-// int count_occurrences(int n, int arr[], int target)
-// {
-//     int cnt = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (arr[i] == target)
-//         {
-//             cnt += 1;
-//         }
-//     }
-//     return cnt;
-// }
+int count_occurrences(int n, int arr[], int target)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            cnt += 1;
+        }
+    }
+    return cnt;
+}
 
 int main()
 {
-        // ****
-    // int n;
-    // cin >> n;
-    // int arr[n];
-    // for (int i = 0; i < n; i++)
-    //     cin >> arr[i];
+    // ****
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
     // prob 1
     // precompute
-    // int hash[13] = {0};
-    // for (int i = 0; i < n; i++)
-    // {
-    //     hash[arr[i]] += 1;
-    // }
-    // int target;
-    // cin >> target;
-    // while (target--)
-    // {
-    //     int number;
-    //     cin >> number;
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]] += 1;
+    }
+    int target;
+    cin >> target;
+    while (target--)
+    {
+        int number;
+        cin >> number;
 
-    //     // fetch
-    //     cout << hash[number] << endl;
-    // }
+        // fetch
+        cout << hash[number] << endl;
+    }
 
-    // cout << count_occurrences(n, arr, target);
+    cout << count_occurrences(n, arr, target);
 
     // ****
 
     // prob 2
-    // string s;
-    // cin>>s;
-    // // precompute
-    // int hash[26]={0};
-    // for(int i=0; i<s.size(); i++) hash[s[i]-'A']++;
+    string s;
+    cin >> s;
+    // precompute
+    int hash[26] = {0};
+    for (int i = 0; i < s.size(); i++)
+        hash[s[i] - 'A']++;
 
-    // int q;
-    // cin>>q;
-    // while(q--){
-    //     char c;
-    //     cin>>c;
-    //     // fetch
-    //     cout<<hash[c-'A']<<endl;
-    // }
-
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        char c;
+        cin >> c;
+        // fetch
+        cout << hash[c - 'A'] << endl;
+    }
 
     //* map
-    
 
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i =0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    // precompute
 
-     unordered_map<int,int> mpp;
-    for(int i=0; i<n;i++){
+    // precompute
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
         mpp[arr[i]]++;
     }
 
     // for how it store in sorted order
-    for(auto it: mpp){
-        cout<<it.first<<" ->"<<it.second<<endl;
+    for (auto it : mpp)
+    {
+        cout << it.first << " ->" << it.second << endl;
     }
 
     int q;
-    cin>>q;
-    while(q--){
+    cin >> q;
+    while (q--)
+    {
         int num;
-        cin>>num;
+        cin >> num;
         // fetch
-        cout<<mpp[num]<<endl;
+        cout << mpp[num] << endl;
     }
-
-    
 
     return 0;
 }
