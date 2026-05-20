8ca22de0723e4b1c80feff3d4edcbd604f124623
SOUNAK GHOSH <sounak2309@gmail.com>
2026-05-20 21:14:43 +0530

    maxumum subarray problem solved

---
+++ 03_Arrays/2.Medium/04.Kadane's_algorithm.cpp
@@ -0,0 +1,48 @@
+/*
+QUESTION:-https://leetcode.com/problems/maximum-subarray/
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+*/
+
+/*
+OPTIMAL APPROACH KADANE'S ALGORITHM:-
+-> Initialize two variables: maxSum and currentSum. Set both variables to the first element of the array.
+-> Iterate through the array starting from the second element:
+    Update currentSum by adding the current element to it.
+    If currentSum becomes negative, reset it to 0. This step ensures that we consider only the subarrays with positive sums.
+    Update maxSum by taking the maximum value between maxSum and currentSum. This keeps track of the maximum subarray sum encountered so far.
+-> After the iteration, the maxSum variable will hold the largest sum of any subarray.
+-> Return the maxSum as the result.
+*/
+
+// CODE:-
+#include <bits/stdc++.h>
+using namespace std;
+int maxSubArray(vector<int> &nums)
+{
+    int sum = 0;
+    int maxi = INT_MIN;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+        maxi = max(maxi, sum);
+        // maxi = sum
+        if (sum < 0)
+            sum = 0;
+    }
+    return maxi;
+}
+
+// TIME COMPLEXITY = O(N)
+// SPACE COMPLEXITY = O(0)
