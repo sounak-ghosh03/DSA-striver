a165feb0d9dcfeb2463a75c327e8a0b6a4320020
SOUNAK GHOSH <sounak2309@gmail.com>
2026-07-28 10:46:36 +0530

    sqrt of number

---
+++ 04_Binary Search/BS on Search Space/01.Square_root_of_number.cpp
@@ -0,0 +1,56 @@
+/*
+QUESTION:
+Given an integer x, find the square root of x. If x is not a perfect square, then return floor(√x).
+
+Example:
+
+Input:
+x = 5
+Output: 2
+Explanation: Since 5 is not a perfect square, the floor of the square root of 5 is 2.
+
+Input:
+x = 4
+Output: 2
+Explanation: Since 4 is a perfect square, the square root of 4 is 2.
+
+APPROACH:
+We can use binary search to find the square root of x.
+1. Initialize the search space with low = 1 and high = x.
+2. While low is less than or equal to high:
+     - Calculate the mid of the search space.
+     - If the square of mid is equal to x, return mid as the square root.
+     - If the square of mid is less than x, update the answer to mid and set low = mid + 1 to search for a larger value.
+     - If the square of mid is greater than x, set high = mid - 1 to search for a smaller value.
+3. Return the answer, which represents the floor of the square root of x.
+
+CODE:
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int floorSqrt(int n)
+{
+    int low = 1, high = n;
+    int ans = 1;
+    while (low <= high)
+    {
+        long long mid = low + (high - low) / 2;
+        long long val = mid * mid;
+        if (val <= n)
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+/*
+Time Complexity: O(log(x))
+Space Complexity: O(1)
+*/
